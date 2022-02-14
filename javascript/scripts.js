97d8cebc30dea8a703eeff00a78651f162842a1c
Hiago Silva <hiagosilvaanjos7@gmail.com>
2022-02-14 18:37:13 -0300

    Iniciei com javascript

---
+++ javascript/scripts.js
@@ -0,0 +1,5 @@
+const getValueInputAdult = document.querySelector('.input-adults-value');
+const getValueInputkid = document.querySelector('.input-kids-value');
+const getHoursDuration = document.querySelector('.input-duration');
+const buttonStartCalc = document.querySelector('.button-calc');
+
